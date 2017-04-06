ea5024339743e322223337e1fce43d3bcf5eb3fc
Paco Pastor <pastoraznar.francisco@gmail.com>
2017-04-06 17:09:16 +0200

    Añadiendo argumento -favourite

--- pb.c
+++ pb.c
@@ -48,73 +48,49 @@ int calculate_result(int white_balls[5], int power_ball)
 }
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char * * argv) {
     int balls[6];
     int count_balls = 0;
-    bool favourite = false;
+    int favorite = 0;
 
-    for (int i=1; i<argc; i++)
-    {
-        const char* arg = argv[i];
+    for (int i = 1; i < argc; i++) {
+      const char * arg = argv[i];
 
-        if ('-' == arg[0])
-        {
-            if (0 == strcmp(arg, "-favourite"))
-            {
-                favourite = true;
-            }
-            else
-            {
-	      goto usage_error;
-            }
+      if ('-' == arg[0]) {
+        if (0 == strcmp(arg, "-favorite")) {
+          favorite = 1;
+        } else {
+          goto usage_error;
         }
-        else
-        {
-            char* endptr = NULL;
-            long val = strtol(arg, &endptr, 10);
-	    if (*endptr)
-            {
-	      goto usage_error;
-            }
-            balls[count_balls++] = (int) val;
+      } else {
+        char * endptr = NULL;
+        long val = strtol(arg, & endptr, 10);
+        if ( * endptr) {
+          goto usage_error;
         }
+        balls[count_balls++] = (int) val;
+      }
+    }
+    if (6 != count_balls) {
+      goto usage_error;
     }
 
-    if (6 != count_balls)
-    {
-        goto usage_error;
-    }
-
-    // the power ball is always the last one given
     int power_ball = balls[5];
-
-    // calculate result can return -1 if the ball numbers
-    // are out of range
-
     int result = calculate_result(balls, power_ball);
-
-    if (result < 0)
-    {
-        goto usage_error;
+    if (result < 0) {
+      goto usage_error;
     }
-
-    if (LUCKY_NUMBER == power_ball)
-    {
-        result = result * 2;
+    if (LUCKY_NUMBER == power_ball) {
+      result = result * 2;
     }
-
-    if (favourite)
-    {
-        result = result * 2;
+    if (favorite) {
+      result = result * 2;
     }
 
     printf("%d percent chance of winning\n", result);
-
     return 0;
-
     usage_error:
-    fprintf(stderr, "Usage: %s [-favourite] (5 white balls) power_ball\n", argv[0]);
-    return -1;
-}
-
+      fprintf(stderr, "Usage: %s [-favorite] (5 white balls)
+        power_ball\n ", argv[0]);
+        return -1;
+      }
